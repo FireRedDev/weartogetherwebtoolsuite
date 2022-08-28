fadd826250d04b1a969862264aad518cdfe824f8
chrisi.gusenbauer101@gmail.com <chrisi.gusenbauer101@gmail.com>
2022-08-28 19:41:41 +0200

    initial

---
+++ wear_together_toolsuite/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from wear_together_toolsuite.users.models import User
+from wear_together_toolsuite.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
